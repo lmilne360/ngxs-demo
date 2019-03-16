4e7fc0349e3656aaa24baf6addc501aef971100d
Laurent <lmilne360@gmail.com>
2019-03-15 23:21:24 -0500

    added order actions to app state

--- src/app/shared/app.state.ts
+++ src/app/shared/app.state.ts
@@ -1,21 +1,44 @@
-import { State, Action, StateContext,  } from '@ngxs/store';
-import { SetUsername } from './app.actions';
+import { Action, State, StateContext } from '@ngxs/store';
+import { tap } from 'rxjs/operators';
+import { OrderService } from '../services/order.service';
+import { ConfirmOrder, OrderFailed, OrderSuccess, SetUsername } from './app.actions';
 
 export interface AppStateModel {
   username: string;
   orderId: number;
   status?: 'pending'| 'confirmed' | 'declined';
 }
-@State<AppStateModel>({name: 'app',
+@State<AppStateModel>({
+  name: 'app',
   defaults: {
     username: '',
     orderId: Math.floor(Math.random() * 23000)
   }
 })
 export class AppState {
+  constructor(private orderService: OrderService) {}
+
   @Action(SetUsername)
   setUsername({patchState}: StateContext<AppStateModel>,  {payload}: SetUsername) {
     patchState({username: payload});
 
+  }
+
+  @Action(ConfirmOrder, {cancelUncompleted: true})
+  confirm({dispatch, patchState}: StateContext<AppStateModel>) {
+    patchState({status: 'pending'});
+    return  this.orderService.post().pipe(
+      tap(success => (success ? dispatch(OrderSuccess) : dispatch(OrderFailed)))
+    );
+  }
+
+  @Action(OrderSuccess)
+  OrderSuccess({patchState}: StateContext<AppStateModel>) {
+    patchState({status: 'confirmed'});
+
+  }
+  @Action(OrderFailed)
+  orderFailed({patchState}: StateContext<AppStateModel>) {
+    patchState({status: 'declined'});
   }
 }
