5932643b67669ee268e90bb9881c32140e8b3080
Laurent <lmilne360@gmail.com>
2019-03-15 21:57:44 -0500

    setup routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {path: 'salad', loadChildren: './salad/salad.module#SaladModule'}
+];
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
