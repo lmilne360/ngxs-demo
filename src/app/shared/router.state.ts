65a3c00fc739b0f671a2fc59260aada3b21c20c2
Laurent <lmilne360@gmail.com>
2019-03-15 21:34:40 -0500

    set up router state

--- src/app/shared/router.state.ts
+++ src/app/shared/router.state.ts
@@ -1,8 +1,32 @@
-import { State } from '@ngxs/store';
+import { Router } from '@angular/router';
+import { Action, State, StateContext } from '@ngxs/store';
 
-interface RouterStateModel {}
-@State<RouterStateModel>({
-  defaults: {
-  }
+export class Navigate {
+  static readonly type = '[router] navigate';
+  constructor(public payload: string) {}
+}
+
+@State<string>({
+  name: 'router',
+  defaults: ''
 })
-export class RouterState {}
+export class RouterState {
+  constructor(private router: Router) {}
+
+  @Action(Navigate)
+  async changeRoute(context: StateContext<string>, action: Navigate) {
+    const path = action.payload;
+    await this.router.navigate([path]);
+    context.setState(path);
+  }
+}
+
+
+// /* This is how to do it in the rxjs effects pattern */
+// @Injectable()
+// export class RouteHandler {
+//  constructor(private router: Router, private actions$: Actions) {
+//    this.actions$.pipe(ofAction(Navigate))
+//    .subscribe(({payload}) => this.router.navigate([payload]));
+//  }
+// }
