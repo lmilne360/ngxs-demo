769ff764ffaf396d721b8ea66ea0859b1fae6489
Laurent <lmilne360@gmail.com>
2019-03-15 23:09:03 -0500

    updated order service

---
+++ src/app/services/order.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+
+@Injectable()
+export class OrderService {
+  constructor() {}
+
+  post() {
+    return of(Math.random() >= 0.5).pipe(delay(2000));
+  }
+}
